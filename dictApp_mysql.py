4c8a1fcc82b1396ce1756ab0e98af202398e555b
Floneyyang <floneyyang@gmail.com>
2021-01-19 20:38:03 -0500

    mute password

--- dictApp_mysql.py
+++ dictApp_mysql.py
@@ -3,10 +3,10 @@ import json
 from difflib import get_close_matches
 
 con = mysql.connector.connect(
-user = "ardit700_student",
-password = "ardit700_student",
-host = "108.167.140.122",
-database = "ardit700_pm1database"
+user = "...",
+password = "...",
+host = "...",
+database = "..."
 )
 
 data = json.load(open("data.json"))
